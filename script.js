79c13c589d9a98250d98c54447b16c63fa0170e1
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-06-22 22:46:31 +0000

    Update CSS and add event listener
    Added a class of box to the CSS file with style properties. Removed the
    defined styles in the for and and added a class of box to the div
    element. Added event listener to the grid container to target it's
    childrenelements

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ const numberOfGridBoxes = 532;
 
 for (let i = 0; i < numberOfGridBoxes; i++) {
     const gridBox = document.createElement('div');
-    gridBox.style.width = '16px';
-    gridBox.style.height = '16px';
-    gridBox.style.border = '1px solid black';
+    gridBox.classList.add('box');
 
     gridContainer.appendChild(gridBox);
 }
+
+gridContainer.addEventListener('mousemove', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('box')) {
+        target.style.backgroundColor = 'black';
+    }
+});
