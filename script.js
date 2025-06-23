f0fe08256b74fbc75fa3d92921594e430a326b95
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-06-23 02:31:54 +0000

    Refactor HTML CSS and JavaScript file
    Created a function to build the grid boxes. Added a heading and a button
    to the HTML file. Rewrote the styles for the grid container and the box
    class

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const gridContainer = document.querySelector('#grid-container');
 
-const numberOfGridBoxes = 532;
+// Width of grid container in CSS file
+const containerWidth = 500;
 
-for (let i = 0; i < numberOfGridBoxes; i++) {
-    const gridBox = document.createElement('div');
-    gridBox.classList.add('box');
-
-    gridContainer.appendChild(gridBox);
+function buildGridBox(rows, cols) {
+    for (let i = 0; i < (rows * cols); i++) {
+        const gridBox = document.createElement('div');
+        gridBox.classList.add('box');
+        gridBox.style.width = `${containerWidth/cols}px`;
+        gridBox.style.height = `${containerWidth/rows}px`;
+        gridContainer.appendChild(gridBox);
+    }
 }
+
+buildGridBox(16, 16);
 
 gridContainer.addEventListener('mousemove', (event) => {
     const target = event.target;
